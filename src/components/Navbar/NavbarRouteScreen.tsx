20de0e9c66cf835387b72734edeacc6056aa03f5
Denis Sologub <contedevel2010@gmail.com>
2022-02-19 02:24:02 +0300

    fix(navbar): fix errors

--- src/components/Navbar/NavbarRouteScreen.tsx
+++ src/components/Navbar/NavbarRouteScreen.tsx
@@ -1,5 +1,27 @@
-import BottomNavigationRouteScreen from '../BottomNavigation/BottomNavigationRouteScreen';
+import React from 'react';
+import { Animated, View, ViewProps } from 'react-native';
 
-const NavbarRouteScreen = BottomNavigationRouteScreen;
+interface Props extends ViewProps {
+  visibility?: 0 | 1 | Animated.AnimatedInterpolation;
+  index: number;
+}
 
-export default NavbarRouteScreen;
+class NavbarRouteScreen extends React.Component<Props> {
+  render(): JSX.Element {
+    const { style, index, children, visibility, ...rest } = this.props;
+
+    const display = visibility === 0 ? 'none' : 'flex';
+
+    return (
+      <View
+        testID={`RouteScreen: ${index}`}
+        style={[style, { display }]}
+        {...rest}
+      >
+        {children}
+      </View>
+    );
+  }
+}
+
+export default Animated.createAnimatedComponent(NavbarRouteScreen);
